3ed714f17038611bc8bafd9440760e0e684e548b
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2025-09-03 17:24:07 +0300

    Initial commit

---
+++ WebSocket/WebSocket/WebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketApp.swift
+//  WebSocket
+//
+//  Created by Ashwaq Alghamdi on 3.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
